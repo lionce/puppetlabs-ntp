a5e7c0e3a6d28f352ad8259a125d87932e63cdd8
lionce <loredana.ionce@puppet.com>
2019-07-17 17:46:23 +0300

    fix daemon test

--- spec/acceptance/ntp_daemon_opts_spec.rb
+++ spec/acceptance/ntp_daemon_opts_spec.rb
@@ -1,26 +1,48 @@
 require 'spec_helper_acceptance'
 
+case os[:family]
+when 'redhat', 'freebsd', 'linux'
+  servicename = 'ntpd'
+else
+  servicename = if os[:family] == 'sles' && os[:release].start_with?('12', '15')
+                  'ntpd'
+                else
+                  'ntp'
+                end
+end
+
 config = if os[:family] == 'redhat'
-           '/etc/sysconfig/ntp'
+           "/etc/sysconfig/#{servicename}"
          else
-           '/etc/default/ntp'
+           "/etc/default/#{servicename}"
          end
 
-describe 'ntp class with daemon options:', unless: UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
+puts os[:family]
+describe 'ntp class with daemon options:', unless: UNSUPPORTED_PLATFORMS.include?(os[:family]) do
+  let(:pp) do
+    "class { 'ntp':
+    service_enable => true,
+    service_ensure => running,
+    service_manage => true,
+    service_name   => '#{servicename}',
+    user => 'ntp',
+    daemon_extra_opts => '-g -i /var/lib/ntp' }"
+  end
+
   it 'run module twice idempotently' do
-    idempotent_apply(default, pp, {})
+    idempotent_apply(pp)
   end
 
   describe file(config.to_s) do
     its(:content) { is_expected.to match(%r{(OPTIONS|NTPD_OPTS)='-g -i \/var\/lib\/ntp'}) }
   end
 
-  if fact('osfamily') == 'redhat'
+  if os[:family] == 'redhat'
     describe file('/etc/systemd/system/multi-user.target.wants/ntpd.service') do
       its(:content) { is_expected.to match(%r{ntpd -u ntp:ntp}) }
     end
   end
-  if fact('osfamily') == 'debian'
+  if os[:family] == 'debian'
     describe file('/usr/lib/ntp/ntp-systemd-wrapper') do
       its(:content) { is_expected.to match(%r{RUNASUSER=ntp}) }
     end
